6b2d5eb77f54d960161b45de2c4ce79ab8e3362b
cake1009 <sum19@naver.com>
2019-01-20 19:37:07 +0900

    CNN

--- mnist.py
+++ mnist.py
@@ -1,69 +1,84 @@
 import tensorflow as tf
 import random
-import matplotlib.pyplot as plt
-tf.set_random_seed(777)
-
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-nb_classes = 10
-
-X1 = tf.placeholder(tf.float32, [None, 784])
-X2 = tf.placeholder(tf.float32, [None, 49])
-Y = tf.placeholder(tf.float32, [None, nb_classes])
-
-W1 = tf.get_variable('W1', shape = [784, 49], initializer = tf.contrib.layers.xavier_initializer())
-W2 = tf.get_variable('W2', shape = [49, nb_classes], initializer = tf.contrib.layers.xavier_initializer())
-b1 = tf.Variable(tf.random_normal([49]))
-b2 = tf.Variable(tf.random_normal([nb_classes]))
-
-X2 = tf.nn.relu(tf.add(tf.matmul(X1, W1), b1))
-hypothesis = tf.add(tf.matmul(X2, W2), b2)
-
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
-    logits = hypothesis, labels = Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=0.005).minimize(cost)
-
-is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-
+learning_rate = 0.001
 training_epochs = 15
 batch_size = 100
-saver = tf.train.Saver()
 
-min_loss = 10000
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+keep_prob = tf.placeholder(tf.float32)
 
-    for epoch in range(training_epochs):
-        avg_cost = 0
-        total_batch = int(mnist.train.num_examples / batch_size)
+X = tf.placeholder(tf.float32, [None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])
+Y = tf.placeholder(tf.float32, [None, 10])
 
-        for i in range(total_batch):
-            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-            c, _ = sess.run([cost, optimizer], feed_dict={X1: batch_xs, Y: batch_ys})
-            avg_cost += c / total_batch
+W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
+L1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+L1 = tf.nn.relu(L1)
+L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L1 = tf.nn.dropout(L1, keep_prob=keep_prob)
 
-        print('Epoch : %04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
-        
-        acc, loss = sess.run([accuracy, cost], feed_dict = {X1: mnist.validation.images, Y: mnist.validation.labels})
-        print("validation acc : {:.2%}, loss : {:.5f}".format(acc, loss))
-        if min_loss > loss:
-            min_loss = loss
-            saver.save(sess, "./save.ckpt")
-            print("Session saved")
-        print("")
+W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
+L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+L2 = tf.nn.relu(L2)
+L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
+                    strides=[1, 2, 2, 1], padding='SAME')
+L2 = tf.nn.dropout(L2, keep_prob=keep_prob)
 
-    print("Learning finished")
+W3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+L3 = tf.nn.conv2d(L2, W3, strides=[1, 1, 1, 1], padding='SAME')
+L3 = tf.nn.relu(L3)
+L3 = tf.nn.max_pool(L3, ksize=[1, 2, 2, 1], strides=[
+                    1, 2, 2, 1], padding='SAME')
+L3 = tf.nn.dropout(L3, keep_prob=keep_prob)
+L3_flat = tf.reshape(L3, [-1, 128 * 4 * 4])
 
-    saver.restore(sess, "./save.ckpt")
-    print("Train Accuracy: {:.2%}".format(accuracy.eval(session=sess, feed_dict={X1: mnist.train.images, Y: mnist.train.labels})))
-    print("Valid Accuracy: {:.2%}".format(accuracy.eval(session=sess, feed_dict={X1: mnist.validation.images, Y: mnist.validation.labels})))
-    print("Test Accuracy: {:.2%}".format(accuracy.eval(session=sess, feed_dict={X1: mnist.test.images, Y: mnist.test.labels})))
+W4 = tf.get_variable("W4", shape=[128 * 4 * 4, 625],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([625]))
+L4 = tf.nn.relu(tf.matmul(L3_flat, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob=keep_prob)
 
-    r = random.randint(0, mnist.test.num_examples - 1)
-    print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
-    print("Prediction: ", sess.run(tf.argmax(hypothesis, 1), feed_dict={X1: mnist.test.images[r:r + 1]}))
+W5 = tf.get_variable("W5", shape=[625, 10],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([10]))
+logits = tf.matmul(L4, W5) + b5
 
-    plt.imshow(mnist.test.images[r:r + 1].reshape(28, 28), cmap='Greys', interpolation='nearest')
-    plt.show()
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+    logits=logits, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        feed_dict = {X: batch_xs, Y: batch_ys, keep_prob: 0.7}
+        c, _ = sess.run([cost, optimizer], feed_dict=feed_dict)
+        avg_cost += c / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning Finished')
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print('Accuracy:', sess.run(accuracy, feed_dict={
+      X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1}))
+
+r = random.randint(0, mnist.test.num_examples - 1)
+print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+print("Prediction: ", sess.run(
+    tf.argmax(logits, 1), feed_dict={X: mnist.test.images[r:r + 1], keep_prob: 1}))
+
+plt.imshow(mnist.test.images[r:r + 1].
+           reshape(28, 28), cmap='Greys', interpolation='nearest')
+plt.show()
